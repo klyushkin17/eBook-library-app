794795411da5d3f9b3b69e7623bc46335118635b
klyushkin <stepan.9h88@mail.ru>
2024-07-14 16:50:25 +0300

    Update SignInScreen UI

--- app/src/main/java/com/example/e_book_libruary_app/presentation/sign_in/SignInScreen.kt
+++ app/src/main/java/com/example/e_book_libruary_app/presentation/sign_in/SignInScreen.kt
@@ -1,18 +1,41 @@
 package com.example.e_book_libruary_app.presentation.sign_in
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.rememberImagePainter
+import com.example.e_book_libruary_app.R
+import com.example.e_book_libruary_app.ui.theme.darkGreen
+import com.example.e_book_libruary_app.ui.theme.green
+import com.example.e_book_libruary_app.ui.theme.harunoUmiFontFamily
+import com.example.e_book_libruary_app.ui.theme.lightGreen
+import com.example.e_book_libruary_app.ui.theme.white
 import com.google.android.gms.fido.u2f.api.common.ChannelIdValue.UnsupportedChannelIdValueTypeException
 
 @Composable
@@ -30,18 +53,39 @@ fun SignInScreen(
             ).show()
         }
     }
-    
-    Box(
+
+    Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
-        contentAlignment = Alignment.Center
+            .background(color = lightGreen),
+        Arrangement.Center,
+        Alignment.CenterHorizontally
     ){
-        Text(text = "Ready to read?")
+        Image(
+            modifier = Modifier
+                .height(120.dp),
+            painter = rememberImagePainter(data = R.drawable.greeting_image),
+            contentDescription = "greeting image",
+            contentScale = ContentScale.FillHeight
+        )
+        Spacer(modifier = Modifier.height(20.dp))
         Button(
-            onClick = onSignInClick
+            shape = RoundedCornerShape(12.dp),
+            colors = ButtonDefaults.buttonColors(darkGreen),
+            onClick = {
+                onSignInClick()
+            },
+            modifier = Modifier
+                .height(44.dp)
+                .width(140.dp)
         ) {
-            Text(text = "Sign in")
+            Text(
+                text = "Sign in",
+                fontFamily = harunoUmiFontFamily,
+                fontWeight = FontWeight.Normal,
+                fontSize = 15.sp,
+                color = white
+            )
         }
     }
 }
--- app/src/main/java/com/example/e_book_libruary_app/ui/theme/Type.kt
+++ app/src/main/java/com/example/e_book_libruary_app/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.example.e_book_libruary_app.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.e_book_libruary_app.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -31,4 +33,10 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+
+val harunoUmiFontFamily = FontFamily(
+    Font(R.font.kaiseiharunoumi_bold, FontWeight.Bold),
+    Font(R.font.kaiseiharunoumi_medium, FontWeight.Medium),
+    Font(R.font.kaiseiharunoumi_regular, FontWeight.Normal)
 )
